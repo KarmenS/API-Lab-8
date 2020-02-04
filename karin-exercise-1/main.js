5e9a574d93460a5f1cbd2cee2c96d97563498487
Karin Ollermarke <karin.ollermark@gmail.com>
2020-02-04 22:43:57 +0100

    karin-exercise-1

--- karin-exercise-1/main.js
+++ karin-exercise-1/main.js
@@ -1,12 +1,25 @@
 //Same as: import {tween, styler} from 'popmotion'
-const { tween, styler } = window.popmotion;
+const { styler, spring, listen, pointer, value } = window.popmotion;
 
-const element = document.getElementsByClassName("rectangle")[0];
+const ball = document.getElementsByClassName("rectangle")[0];
 
-const ball = styler(element); 
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
 
-tween({to: 300, duration: 500 }).start(
-    function (value) { 
-        ball.set('x', value);
-    }
-    );
+listen(ball, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: ballXY.get(),
+      velocity: ballXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 200,
+      // mass: 1,
+      // damping: 10
+    }).start(ballXY);
+  });
