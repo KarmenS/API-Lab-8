0470b8f003c6dbb306b38f87dff14282f256869b
Miroslav <gtifsi@hotmail.com>
2020-02-05 20:37:54 +0100

    textfield-ref-example

--- Miroslav-exercise-1/main.js
+++ Miroslav-exercise-1/main.js
@@ -1,12 +1,50 @@
 //Same as: import {tween, styler} from 'popmotion'
-const { tween, styler } = window.popmotion;
+const { tween, styler, spring, value, transform, listen, valueTypes } = window.popmotion;
+const { blendColor, clamp, interpolate, pipe } = transform;
 
-const element = document.getElementsByClassName("rectangle")[0];
+// Select elements
+const input = document.querySelector('.input-container input');
+const counter = document.querySelector('.input-container label');
 
-const ball = styler(element); 
+// Setup styler and scale `value`
+const counterStyler = styler(counter);
+const counterScale = value(1, counterStyler.set('scale'));
+const charLimit = parseInt(counter.innerHTML);
 
-tween({to: 30, duration: 50 }).start(
-    function (value) { 
-        ball.set('x', value);
-    }
-    );
+const fireSpring = () => spring({
+    from: counterScale.get(),
+    to: 1,
+    velocity: Math.max(counterScale.getVelocity(), 100),
+    stiffness: 700,
+    damping: 80
+  }).start(counterScale);
+
+  const convertCharCountToColor = pipe(
+    clamp(charLimit * 0.5, charLimit),
+    
+    // Map character count to a 0-1 range
+    interpolate([charLimit * 0.5, charLimit], [0, 1]),
+    
+    // Use that number to blend grey and red
+    blendColor(counterStyler.get('color'), '#f00')
+  );
+
+  const updateRemainingCharsCounter = (val) => {
+    // Measure char count
+    const charCount = val.length;
+  
+    // Set remaining chars
+    counter.innerHTML = charLimit - charCount;
+  
+    // Set counter color
+    counterStyler.set('color', convertCharCountToColor(charCount));
+  };
+  
+  listen(input, 'keydown')
+    .filter(({ target }) => target.value.length === charLimit)
+    .start(fireSpring);
+  
+  listen(input, 'keyup')
+    .pipe(({ target }) => target.value)
+    .start(updateRemainingCharsCounter);
+    
