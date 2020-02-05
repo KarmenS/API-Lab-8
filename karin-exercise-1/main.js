adea716cfe35f8643b828859f06dab9492fa853e
Karin Ollermarke <karin.ollermark@gmail.com>
2020-02-06 00:16:37 +0100

    Update main.js

--- karin-exercise-1/main.js
+++ karin-exercise-1/main.js
@@ -1,74 +1,111 @@
 //Same as: import {tween, styler} from 'popmotion'
 const { styler, spring, listen, pointer, value } = window.popmotion;
 
-// ball one
-const ball1 = document.getElementsByClassName("rectangle1")[0];
+// create rectangle 1
+const rectangle1 = document.getElementsByClassName("rectangle1")[0];
 
-const divStyler1 = styler(ball1);
+const divStyler1 = styler(rectangle1);
 
-const ball1XY = value({ x: 0, y: 0 }, divStyler1.set);
+const rectangle1XY = value({ x: 0, y: 0 }, divStyler1.set);
 
-listen(ball1, 'mousedown touchstart')
+listen(rectangle1, 'mousedown touchstart')
   .start((e) => {
     e.preventDefault();
-    pointer(ball1XY.get()).start(ball1XY);
+    pointer(rectangle1XY.get()).start(rectangle1XY);
   });
 
 listen(document, 'mouseup touchend')
   .start(() => {
-    ball1XY.stop();
+    rectangle1XY.stop();
   });
 
-//ball two
-const ball2 = document.getElementsByClassName("rectangle2")[0];
+// create rectangle 2
+const rectangle2 = document.getElementsByClassName("rectangle2")[0];
 
-const divStyler2 = styler(ball2);
+const divStyler2 = styler(rectangle2);
 
-const ball2XY = value({ x: 0, y: 0 }, divStyler2.set);
+const rectangle2XY = value({ x: 0, y: 0 }, divStyler2.set);
 
-listen(ball2, 'mousedown touchstart')
+listen(rectangle2, 'mousedown touchstart')
   .start((e) => {
     e.preventDefault();
-    pointer(ball2XY.get()).start(ball2XY);
+    pointer(rectangle2XY.get()).start(rectangle2XY);
   });
 
 listen(document, 'mouseup touchend')
   .start(() => {
-    ball2XY.stop();
+    rectangleXY.stop();
   });
 
-//ball three
-const ball3 = document.getElementsByClassName("rectangle3")[0];
+// create rectangle 3
+const rectangle3 = document.getElementsByClassName("rectangle3")[0];
 
-const divStyler3 = styler(ball3);
+const divStyler3 = styler(rectangle3);
 
-const ball3XY = value({ x: 0, y: 0 }, divStyler3.set);
+const rectangle3XY = value({ x: 0, y: 0 }, divStyler3.set);
 
-listen(ball3, 'mousedown touchstart')
+listen(rectangle3, 'mousedown touchstart')
   .start((e) => {
     e.preventDefault();
-    pointer(ball3XY.get()).start(ball3XY);
+    pointer(rectangle3XY.get()).start(rectangle3XY);
   });
 
 listen(document, 'mouseup touchend')
   .start(() => {
-    ball3XY.stop();
+    rectangle3XY.stop();
   });
 
-//ball four
-const ball4 = document.getElementsByClassName("rectangle4")[0];
+// create rectangle 4
+const rectangle4 = document.getElementsByClassName("rectangle4")[0];
 
-const divStyler4 = styler(ball4);
+const divStyler4 = styler(rectangle4);
 
-const ball4XY = value({ x: 0, y: 0 }, divStyler4.set);
+const rectangle4XY = value({ x: 0, y: 0 }, divStyler4.set);
 
-listen(ball4, 'mousedown touchstart')
+listen(rectangle4, 'mousedown touchstart')
   .start((e) => {
     e.preventDefault();
-    pointer(ball4XY.get()).start(ball4XY);
+    pointer(rectangle4XY.get()).start(rectangle4XY);
   });
 
 listen(document, 'mouseup touchend')
   .start(() => {
-    ball4XY.stop();
+    rectangle4XY.stop();
+  });
+
+// create triangle 1 
+const triangle1 = document.getElementsByClassName("triangle1")[0];
+
+const divStyler5 = styler(triangle1);
+
+const triangle1XY = value({ x: 0, y: 0 }, divStyler5.set);
+
+listen(triangle1, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(triangle1XY.get()).start(triangle1XY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    triangle1XY.stop();
+  });
+
+
+// create triangle 2  
+const triangle2 = document.getElementsByClassName("triangle2")[0];
+
+const divStyler6 = styler(triangle2);
+
+const triangle2XY = value({ x: 0, y: 0 }, divStyler6.set);
+
+listen(triangle2, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(triangle2XY.get()).start(triangle2XY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    triangle2XY.stop();
   });
