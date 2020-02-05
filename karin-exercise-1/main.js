d43ab5a9e395eb692e9d0ebddd375bf49133691e
Karin Ollermarke <karin.ollermark@gmail.com>
2020-02-06 00:28:09 +0100

    karin-exercise-1

--- karin-exercise-1/main.js
+++ karin-exercise-1/main.js
@@ -2,34 +2,16 @@
 const { styler, spring, listen, pointer, value } = window.popmotion;
 
 // create rectangle 1
-const rectangle1 = document.getElementsByClassName("rectangle1")[0];
+const rectangle = document.getElementsByClassName("rectangle");
 
-const divStyler1 = styler(rectangle1);
+const divStyler = styler(rectangle);
 
-const rectangle1XY = value({ x: 0, y: 0 }, divStyler1.set);
+const rectangleXY = value({ x: 0, y: 0 }, divStyler.set);
 
-listen(rectangle1, 'mousedown touchstart')
+listen(rectangle, 'mousedown touchstart')
   .start((e) => {
     e.preventDefault();
-    pointer(rectangle1XY.get()).start(rectangle1XY);
-  });
-
-listen(document, 'mouseup touchend')
-  .start(() => {
-    rectangle1XY.stop();
-  });
-
-// create rectangle 2
-const rectangle2 = document.getElementsByClassName("rectangle2")[0];
-
-const divStyler2 = styler(rectangle2);
-
-const rectangle2XY = value({ x: 0, y: 0 }, divStyler2.set);
-
-listen(rectangle2, 'mousedown touchstart')
-  .start((e) => {
-    e.preventDefault();
-    pointer(rectangle2XY.get()).start(rectangle2XY);
+    pointer(rectangleXY.get()).start(rectangleXY);
   });
 
 listen(document, 'mouseup touchend')
@@ -37,75 +19,3 @@ listen(document, 'mouseup touchend')
     rectangleXY.stop();
   });
 
-// create rectangle 3
-const rectangle3 = document.getElementsByClassName("rectangle3")[0];
-
-const divStyler3 = styler(rectangle3);
-
-const rectangle3XY = value({ x: 0, y: 0 }, divStyler3.set);
-
-listen(rectangle3, 'mousedown touchstart')
-  .start((e) => {
-    e.preventDefault();
-    pointer(rectangle3XY.get()).start(rectangle3XY);
-  });
-
-listen(document, 'mouseup touchend')
-  .start(() => {
-    rectangle3XY.stop();
-  });
-
-// create rectangle 4
-const rectangle4 = document.getElementsByClassName("rectangle4")[0];
-
-const divStyler4 = styler(rectangle4);
-
-const rectangle4XY = value({ x: 0, y: 0 }, divStyler4.set);
-
-listen(rectangle4, 'mousedown touchstart')
-  .start((e) => {
-    e.preventDefault();
-    pointer(rectangle4XY.get()).start(rectangle4XY);
-  });
-
-listen(document, 'mouseup touchend')
-  .start(() => {
-    rectangle4XY.stop();
-  });
-
-// create triangle 1 
-const triangle1 = document.getElementsByClassName("triangle1")[0];
-
-const divStyler5 = styler(triangle1);
-
-const triangle1XY = value({ x: 0, y: 0 }, divStyler5.set);
-
-listen(triangle1, 'mousedown touchstart')
-  .start((e) => {
-    e.preventDefault();
-    pointer(triangle1XY.get()).start(triangle1XY);
-  });
-
-listen(document, 'mouseup touchend')
-  .start(() => {
-    triangle1XY.stop();
-  });
-
-
-// create triangle 2  
-const triangle2 = document.getElementsByClassName("triangle2")[0];
-
-const divStyler6 = styler(triangle2);
-
-const triangle2XY = value({ x: 0, y: 0 }, divStyler6.set);
-
-listen(triangle2, 'mousedown touchstart')
-  .start((e) => {
-    e.preventDefault();
-    pointer(triangle2XY.get()).start(triangle2XY);
-  });
-
-listen(document, 'mouseup touchend')
-  .start(() => {
-    triangle2XY.stop();
-  });
