5d3509b41c58f4676bc971cb0e3c5cab957eab1c
Miskajela <michaela.poprocka@gmail.com>
2020-02-05 12:30:49 +0100

    some new movements

--- michaela-template/main.js
+++ michaela-template/main.js
@@ -9,12 +9,61 @@ const ballStyler = styler(circle);
 
 let arrayOfElements = [rectangleStyler, ballStyler]
 
+let mouseX;
+let mouseY;
+
+const rectangleY = rectangle.style.top
+
+console.log(rectangleY)
+      
+
+let arrayOfPositions = [1000, 1000];
+
+console.log(arrayOfPositions);
+
+function getMousePosition(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    for(let i = 0; i < 1; i++) {
+      arrayOfPositions.push(mouseX, mouseY)
+
+      tween({
+        from: { x: 0, y: 0, scale: 1 },
+        to: { x: 300, y: 250, scale: arrayOfElements.length },
+        ease: easing.easeInOut,
+        flip: Infinity,
+        duration: arrayOfPositions[0]
+      }).start(v => rectangleStyler.set(v));
+      
+      
+
+      tween({
+        from: { x: 0, scale: 1 },
+        to: { x: 450, scale: arrayOfElements.length + 1 },
+        ease: easing.easeInOut,
+        flip: Infinity,
+        duration: arrayOfPositions[1]
+      }).start(v => ballStyler.set(v));
+
+
+
+      if (arrayOfPositions.length > 2) {
+        arrayOfPositions.length = 0
+        arrayOfPositions.push(mouseX*8, mouseY*8)
+      }
+      console.log(arrayOfPositions)
+    }
+
+
+  }
+
+
 tween({
     from: { x: 0, y: 0, scale: 1 },
     to: { x: 300, y: 250, scale: arrayOfElements.length },
     ease: easing.easeInOut,
     flip: Infinity,
-    duration: 1000
+    duration: arrayOfPositions[0]
   }).start(v => rectangleStyler.set(v));
 
   tween({
@@ -22,5 +71,10 @@ tween({
     to: { x: 450, scale: arrayOfElements.length + 1 },
     ease: easing.easeInOut,
     flip: Infinity,
-    duration: 1500
+    duration: arrayOfPositions[1]
   }).start(v => ballStyler.set(v));
+
+  
+
+  
+  document.addEventListener("mousemove", getMousePosition)
