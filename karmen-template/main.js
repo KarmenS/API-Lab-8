1360112fff42ee8a8049a4b8d374cdb53f22f1d7
Karmen <saare20@hotmail.com>
2020-02-18 13:47:07 +0100

    Tween

--- karmen-template/main.js
+++ karmen-template/main.js
@@ -2,9 +2,10 @@
 const { tween, styler, easing } = window.popmotion;
 
 const element = document.getElementsByClassName("rectangle")[0];
-//const newelement = document.getElementsByClassName("another-rectangle")[0];
+const newElement = document.getElementsByClassName("another-rectangle")[0];
 
 const rectangleStyler = styler(element); 
+const newRectangleStyler = styler (newElement);
 
 tween({
     from: { x: 0, scale: 1 },
@@ -13,3 +14,15 @@ tween({
     flip: Infinity,
     duration: 1000
   }).start(v => rectangleStyler.set(v));
+
+
+  tween({
+    from: 0,
+    to: { x: 300, rotate: 180 },
+    duration: 2000,
+    ease: easing.backOut,
+    flip: Infinity,
+    // elapsed: 500,
+    // loop: 5,
+     yoyo: 5
+  }).start(newRectangleStyler.set);
