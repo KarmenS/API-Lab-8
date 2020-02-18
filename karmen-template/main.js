1fa720b6895625d95b00a68a64762cc81ea66194
Karmen <saare20@hotmail.com>
2020-02-18 13:12:26 +0100

    p

--- karmen-template/main.js
+++ karmen-template/main.js
@@ -1,11 +1,13 @@
 //Same as: import {tween, styler} from 'popmotion'
-const { tween, styler } = window.popmotion;
+const { tween, styler, crossfade } = window.popmotion;
 
 const element = document.getElementsByClassName("rectangle")[0];
+const newelement = document.getElementsByClassName("another-rectangle")[0];
 
 const ball = styler(element); 
 
-tween({to: 30, duration: 50 }).start(
+
+tween({to: 300, duration: 500}).start(
     function (value) { 
         ball.set('x', value);
     }
