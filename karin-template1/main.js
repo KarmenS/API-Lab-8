96c2a434a640e16d0296527fd84ab69be9200459
Karin Ollermarke <karin.ollermark@gmail.com>
2020-02-18 10:31:12 +0100

    karin

--- karin-template1/main.js
+++ karin-template1/main.js
@@ -1,17 +1,17 @@
 //Same as: import {tween, styler} from 'popmotion'
 const { styler, spring, listen, pointer, value} = window.popmotion;
 
-const inTheWay = document.getElementsByClassName("box");
-const divStyler = styler(InTheWay);
-const inTheWayXY = value({ x: 0, y: 0 }, divStyler.set);
+const element = document.getElementsByClassName("box");
+const divStyler = styler(element);
+const elementXY = value({ x: 0, y: 0 }, divStyler.set);
 
-listen(inTheWay, 'mousedown touchstart')
+listen(element, 'mousedown touchstart')
   .start((e) => {
     e.preventDefault();
-    pointer(inTheWayXY.get()).start(inTheWayXY);
+    pointer(elementXY.get()).start(elementXY);
   });
 
 listen(document, 'mouseup touchend')
   .start(() => {
-    inTheWayXY.stop();
+    elementXY.stop();
   });
