86f341e4fef725f169056ccc7e3a52f8a37d2763
Karmen <saare20@hotmail.com>
2020-02-18 13:24:55 +0100

    testing

--- karmen-template/main.js
+++ karmen-template/main.js
@@ -1,14 +1,15 @@
 //Same as: import {tween, styler} from 'popmotion'
-const { tween, styler, crossfade } = window.popmotion;
+const { tween, styler, easing } = window.popmotion;
 
 const element = document.getElementsByClassName("rectangle")[0];
-const newelement = document.getElementsByClassName("another-rectangle")[0];
+//const newelement = document.getElementsByClassName("another-rectangle")[0];
 
-const ball = styler(element); 
+const rectangleStyler = styler(element); 
 
-
-tween({to: 300, duration: 500}).start(
-    function (value) { 
-        ball.set('x', value);
-    }
-    );
+tween({
+    from: { x: 0, scale: 1 },
+    to: { x: 300, scale: 2 },
+    ease: easing.easeInOut,
+    flip: Infinity,
+    duration: 1000
+  }).start(v => rectangleStyler.set(v));
