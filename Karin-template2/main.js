e3b9635f2f2fee5bd4698d0651d4c21f44e78496
Karin Ollermarke <karin.ollermark@gmail.com>
2020-02-19 00:50:01 +0100

    karin2

--- Karin-template2/main.js
+++ Karin-template2/main.js
@@ -2,10 +2,14 @@
 
 const { styler, spring, listen, pointer, value } = window.popmotion;
 
-
 const ball = document.getElementsByClassName('ball')[0];
-const divStyler = styler(ball);
-const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+const ball2 = document.getElementsByClassName('anotherBall')[0];
+
+const ballStyler = styler(ball);
+const ball2Styler = styler(ball2);
+
+const ballXY = value({ x: 20, y: 50 }, ballStyler.set);
+const ball2XY = value({ x: 50, y: 20 }, ball2Styler.set);
 
 listen(ball, 'mousedown touchstart')
   .start((e) => {
@@ -18,9 +22,29 @@ listen(document, 'mouseup touchend')
     spring({
       from: ballXY.get(),
       velocity: ballXY.getVelocity(),
-      to: { x: 0, y: 0 },
-      stiffness: 200,
+      to: { x: 20, y: 50 },
+      stiffness: 10,
       // mass: 1,
       // damping: 10
     }).start(ballXY);
   });
+
+
+  listen(ball2, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ball2XY.get()).start(ball2XY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: ball2XY.get(),
+      velocity: ball2XY.getVelocity(),
+      to: { x: 50, y: 20 },
+      stiffness: 200,
+      // mass: 1,
+      // damping: 10
+    }).start(ball2XY);
+  });
+
