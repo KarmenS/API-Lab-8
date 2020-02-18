6d222d07a28fba4885e1b10c780a35b53f585449
Karin Ollermarke <karin.ollermark@gmail.com>
2020-02-18 10:48:38 +0100

    karin

--- karin-template1/main.js
+++ karin-template1/main.js
@@ -1,7 +1,7 @@
 //Same as: import {tween, styler} from 'popmotion'
 const { styler, spring, listen, pointer, value} = window.popmotion;
 
-const element = document.getElementsByClassName("box");
+const element = document.getElementsByClassName("box")[0];
 const divStyler = styler(element);
 const elementXY = value({ x: 0, y: 0 }, divStyler.set);
 
