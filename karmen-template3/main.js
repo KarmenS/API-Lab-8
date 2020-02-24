4d42ed5aba4e750e1e9e9cd610b6b13f80bacd03
Karmen <saare20@hotmail.com>
2020-02-24 13:56:37 +0100

    Commit

--- karmen-template3/main.js
+++ karmen-template3/main.js
@@ -5,14 +5,6 @@ const element = document.getElementsByClassName("rectangle_5")[0];
 
 const rectangle_5 = styler(element); 
 
-spring({
-    from: 0,
-    to: 250,
-
-    stiffness: 5,
-    damping: 10,
-  })
-  .start(rectangle_5.set);
 
 
 
